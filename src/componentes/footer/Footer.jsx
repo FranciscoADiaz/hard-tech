31bf648e15864f5445a87a1bd57c272e7d7dc9da
Nico Miskevish <nicomiskevish@gmail.com>
2025-03-22 22:30:54 -0300

    Arreglando errores

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -1,80 +1,80 @@
-// Footer.js
-import { NavLink } from "react-router";
-import "./Footer.css";
+import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import { FaInstagram, FaFacebook, FaTwitter, FaTiktok } from "react-icons/fa";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>
-      <footer className="text-withe">
-        <div className="footer">
-          <Container>
-            <nav className="row">
-              <NavLink
-                to="/"
-                className="col-12 col-md-3 d-flex align-items-center justify-content-center pl-3"
-              >
-                <img
-                  src="../../img/LogoHardTech.png"
-                  alt="Logo HardTech"
-                  className="logo-footer"
-                />
-              </NavLink>
-              <ul className="navbar-nav col-12 col-md-3  m-auto py-4 pl-4">
-                <li className="text-center navbar-nav ">
-                  <NavLink
-                    className="fs-5 a"
-                    to="https://www.instagram.com/"
-                    target="_blank"
-                  >
-                    <i className="bi bi-facebook"/> Instagram
-                  </NavLink>
-                </li>
-                <li className="text-center li">
-                  <NavLink
-                    className="fs-5 a"
-                    to="https://www.facebook.com/?locale=es_LA"
-                    target="_blank"
-                  >
-                    Facebook
-                  </NavLink>
-                </li>
-                <li className="text-center ">
-                  <NavLink className="fs-5 a" to="https://x.com/" target="_blank">
-                    X
-                  </NavLink>
-                </li>
-                <li className="text-center li">
-                  <NavLink
-                    className="fs-5 a"
-                    to="https://www.tiktok.com/upload?lang=es"
-                    target="_blank"
-                  >
-                    Tiktok
-                  </NavLink>
-                </li>
-              </ul>
-              <ul class=" navbar-nav col-12 col-md-3 m-auto py-4 ">
-                <li className="text-center navbar-nav">
-                  <NavLink className="fs-5 a" to="#" target="_blank">
-                    Terminos y Condiciones
-                  </NavLink>
-                </li>
-                <li className="text-center">
-                  <NavLink className="fs-5 a" to="#" target="_blank">
-                    Politica de Privacidad
-                  </NavLink>
-                </li>
-                <li className="text-center ">
-                  <NavLink className="fs-5 a" to="#" target="_blank">
-                    Cookies y Publicidad
-                  </NavLink>
-                </li>
-              </ul>
-            </nav>
-          </Container>
+    <footer className="footer">
+      <Container>
+        <div className="footer-content row">
+          <div className="col-12 col-md-4 d-flex align-items-center justify-content-center">
+            <NavLink to="/">
+              <img
+                src="../../img/LogoHardTech.png"
+                alt="Logo HardTech"
+                className="logo-footer"
+              />
+            </NavLink>
+          </div>
+
+          <div className="col-12 col-md-4">
+            <ul className="footer-links">
+              <li>
+                <a
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaInstagram /> Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.facebook.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaFacebook /> Facebook
+                </a>
+              </li>
+              <li>
+                <a href="https://x.com/" target="_blank" rel="noreferrer">
+                  <FaTwitter /> X
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.tiktok.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTiktok /> TikTok
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-12 col-md-4">
+            <ul className="footer-links">
+              <li>
+                <a href="#">Términos y Condiciones</a>
+              </li>
+              <li>
+                <a href="#">Política de Privacidad</a>
+              </li>
+              <li>
+                <a href="#">Cookies y Publicidad</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </footer>
-    </div>
+      </Container>
+
+      {/* Copy Pegado al fondo */}
+      <div className="footer-bottom text-center">
+        © 2025 HardTech. Todos los derechos reservados.
+      </div>
+    </footer>
   );
 }
