ae720972be9884ef16dc1f6e081866556d64ef11
Nico Miskevish <nicomiskevish@gmail.com>
2025-03-23 00:34:45 -0300

    Se arreglo navbar

--- src/componentes/navbar/NavbarC.jsx
+++ src/componentes/navbar/NavbarC.jsx
@@ -1,50 +1,36 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink } from "react-router";
-import "./Navbar.css"
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
 const NavbarC = () => {
   return (
-    <>
-      <Navbar expand="lg" className="navbar">
-        <Container fluid>
-          <NavLink to="/">
-            <img
-              src="https://trello.com/1/cards/67d80f377ad55dc4df25d044/attachments/67db4a0f7016e440b82edf71/download/LOSO_SIN_FONDO_1.png"
-              alt="Logo"
-              className="w-25"
-            />
-          </NavLink>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <NavLink className="a" to="/">
-                Inicio
-              </NavLink>
-              <NavLink className="a" to="/SobreNosotros">
-                Sobre Nosotros
-              </NavLink>
-              <NavLink className="a" to="/Contacto">
-                Contacto
-              </NavLink>
-              <NavLink className="a" to="/Carrito">
-                Carrito
-              </NavLink>
-            </Nav>
+    <Navbar expand="lg" className="navbar" sticky="top">
+      <Container>
+        <NavLink to="/">
+          <img
+            src="https://i.ibb.co/605nwL8L/LOSO-SIN-FONDO-1.png"  
+            alt="Logo"
+            className="logo-navbar"
+          />
+        </NavLink>
 
-            <Nav className="ms-auto">
-              <NavLink className="a" to="/IniciarSesion">
-                Iniciar Sesion
-              </NavLink>
-              <NavLink className="a" to="/Registrarse">
-                Registrarse
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto nav-links">
+            <NavLink to="/" className="nav-item">Inicio</NavLink>
+            <NavLink to="/SobreNosotros" className="nav-item">Sobre Nosotros</NavLink>
+            <NavLink to="/Contacto" className="nav-item">Contacto</NavLink>
+            <NavLink to="/Carrito" className="nav-item">Carrito</NavLink>
+            <NavLink to="/IniciarSesion" className="nav-item">Iniciar Sesión</NavLink>
+            <NavLink to="/Registrarse" className="nav-item">Registrarse</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
 export default NavbarC;
+
