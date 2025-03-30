dc0a21f2db7e11114d003163bf146bd22a690792
toto <totomedrano25@gmail.com>
2025-03-30 19:28:11 -0300

    se agrega funcionalidad al buscador

--- src/componentes/buscador/Buscador.jsx
+++ src/componentes/buscador/Buscador.jsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import "./Buscador.css";
 
-const Buscador = () => {
+const Buscador = ({ onSearch, onCategoryChange }) => {
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    onSearch(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+    onCategoryChange(e.target.value);
+  };
+
   return (
-    <>
-      <section className="search buscador">
-        <input type="search" placeholder="Buscar" className="search-bar" />
-      </section>
-    </>
+    <section className="search buscador">
+      <input
+        type="search"
+        placeholder="Buscar"
+        className="search-bar"
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <select
+        className="category-filter"
+        value={category}
+        onChange={handleCategoryChange}
+      >
+        <option value="">Todas las categorías</option>
+        <option value="laptops">Laptops</option>
+        <option value="monitores">Monitores</option>
+        <option value="perifericos">Periféricos</option>
+        <option value="procesadores">Procesadores</option>
+        <option value="Sillas">Sillas</option>
+        <option value="gabinetes">Gabinetes</option>
+        <option value="mothers">Mothers</option>
+        <option value="memorias">Memorias</option>
+        <option value="joystick">Joysticks</option>
+        <option value="placa de video">Placas de Video</option>
+      </select>
+    </section>
   );
 };
 
